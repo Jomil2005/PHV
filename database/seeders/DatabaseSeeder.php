890babb54cb4795ddbf155aca16d922f27feea2d
Jomil2005 <jomilgruta22@gmail.com>
2025-05-23 04:04:34 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,16 +1,15 @@
-   <?php
+<?php
 
-   namespace Database\Seeders;
+namespace Database\Seeders;
 
-   use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder;
 
-   class DatabaseSeeder extends Seeder
-   {
-       public function run()
-       {
-           // Call your other seeders here
-           $this->call(BarangaySeeder::class);
-           $this->call(RoleUserSeeder::class); // Ensure this matches the actual class name
-       }
-   }
-   
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        // Call your other seeders here
+        $this->call(BarangaySeeder::class);
+        $this->call(RoleUserSeeder::class); // Ensure this matches the actual class name
+    }
+}
